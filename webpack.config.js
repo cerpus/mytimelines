b14fe99adeba45eacd2826daeb849d46be7526a1
Håkon Ellingsen <hakonweb@gmail.com>
2015-10-09 12:53:05 +0200

    Add module `lost`
    It will be used for CSS grids.

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,14 @@ module.exports = {
         loaders: [
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
             // Uncomment when styles are integrated into components
-            // { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!cssnext-loader!postcss-loader') }
-            { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[local]!cssnext-loader!postcss-loader') }
+            // { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!cssnext-loader') }
+            { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[local]!postcss-loader!cssnext-loader') }
         ]
     },
 
     postcss: [
-        require('postcss-nested')
+        require('postcss-nested'),
+        require('lost')
     ],
 
     resolve: {
